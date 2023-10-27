32a4402f4f0bbfdc88bc80cb3b8b89c6875d7b00
GalisGalisteo <galis.travelling@gmail.com>
2023-10-27 14:23:10 +0200

    function paper finished

--- main.js
+++ main.js
@@ -10,28 +10,79 @@
 */
 
 // variable para controlar la puntuación del usuario
-const score = 0;
-const choices = ['rock', 'paper', 'scissors'];
+let score = 0;
 
 
 // funcion aleatoria
+
 function appChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
     const randomNum = Math.floor(Math.random() * 3); // 0, 1, 2
     return choices[randomNum];
 }
 
-function userChoice() {
-    const paperChoice = document.querySelector('.game-body__circle-container-paper');
-    const rockChoice = document.querySelector('.game-body__circle-container-rock');
-    const scissorsChoice = document.querySelector('.game-body__circle-container-scissors');
-    const gameBody = document.querySelector('.game-body__circle-container-scissors');
-
-    paperChoice.addEventListener('click', function () {
-        console.log( choices[1]);
-
-
-    })
-
-
+function userWins(userChoice, appChoice) {
+    return (
+        userChoice === 'rock' && appChoice === 'scissors' ||
+        userChoice === 'paper' && appChoice === 'rock' ||
+        userChoice === 'scissors' && appChoice === 'paper'
+    )
 }
-userChoice();
+
+const paperChoice = document.querySelector('#paper');
+const rockChoice = document.querySelector('#rock');
+const scissorsChoice = document.querySelector('#scissors');
+const gameBody = document.querySelector('.game-body');
+const main = document.querySelector('main');
+
+
+rockChoice.addEventListener('click', function () {
+    const possibleChoices = ['paper', 'scissors'];
+
+    const userChoice = 'rock';
+    let appChoiceVar;
+
+    do {
+        appChoiceVar = appChoice();
+    }
+    while (userChoice === appChoiceVar);
+
+    const possibleChoicesIndex = possibleChoices.indexOf(appChoiceVar);
+    possibleChoices.splice(possibleChoicesIndex, 1);
+
+    rockChoice.classList.add('choosed-item--user');
+    document.getElementById(appChoiceVar).classList.add('choosed-item--com');
+    document.getElementById(possibleChoices[0]).style.display = 'none';
+
+    const gameOverMessage = document.createElement('div');
+    gameOverMessage.classList.add('game-over-container');
+    main.appendChild(gameOverMessage);
+
+    if (!userWins(userChoice, appChoiceVar)) {
+        // devolver mensaje de YOU LOST
+        gameOverMessage.innerHTML = '<h2>YOU LOST!</h2>';
+        // restar 1 punto al score
+        score--;
+    } else {
+        // devolver mensaje de YOU WON
+        gameOverMessage.innerHTML = '<h2>YOU WON!</h2>';
+        // sumar 3 puntos al score
+        score += 3;
+    };
+})
+
+// paperChoice.addEventListener('click', function () {
+//     const userChoice = 'paper';
+//     userWins(userChoice, appChoice());
+// })
+
+// scissorsChoice.addEventListener('click', function () {
+//     const userChoice = 'scissors';
+//     userWins(userChoice, appChoice());
+// })
+
+
+
+
+
+
