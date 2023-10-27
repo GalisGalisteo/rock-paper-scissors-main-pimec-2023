38b1f75b421ee887782f8856dd4e64bd1c9df47b
GalisGalisteo <galis.travelling@gmail.com>
2023-10-27 10:14:58 +0200

    appChoice created

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-// Buena suerte!!
+/** 
+ * LÓGICA DEL JUEGO
+ * 
+ * 1- Crear a la máquina. Tenemos que crear una función que devuelva de forma aleatoria un string: "rock", "paper" or "scissors".
+ * 
+ * 2- Crear función del imput del usuario.
+ * 
+ * 3- Comparar ambas respuestas de las funciones y devolver si ha ganado o perdido
+ * 
+*/
+
+// variable para controlar la puntuación del usuario
+const score = 0;
+
+// funcion aleatoria
+function appChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNum = Math.floor(Math.random() * 3); // 0, 1, 2
+    return choices[randomNum];
+}
+
+function userChoice() {
+    
+}
+
